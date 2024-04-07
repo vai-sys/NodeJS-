47684671a8df1198a329fa1d48489bf738d9afc2
vaishnavi <vaishnavit582@gmail.com>
2024-04-07 16:09:28 +0530

    Connection to database

--- Server.js
+++ Server.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.get('/chicken', function(req, res) {
-    res.send('I love chicken');
-});
-
-app.get('/idli',function(req,res){
-    var customised_idli={
-        name:'idli',
-        is_sambhar:true,
-        is_coconut_chutney:true
-    }
-    res.send(customised_idli);
-})
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
