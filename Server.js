e997670ab7970f47f7412373fb2c892b4e8af03b
vaishnavi <vaishnavit582@gmail.com>
2024-04-07 20:27:25 +0530

    MongoDB connection with server

--- Server.js
+++ Server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
-
-
+const db=require('./db');
+const person=require('./models/person');
+const bodyParser=require('body-parser');
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Welcome,We would like to serve you');
 });
+// post route to add person
+
+app.post('/person',async(req,res)=>{
+  try{
+    const data=req.body;
+    const newPerson=new person(data);
+    const response=await newPerson.save();
+    console.log('data saved');
+    res.status(200).json(response);
+   }
+   catch(err){
+  console.log(err);
+  res.status(500).json({err:'Internal Server Error'})
+   }
+})
+ 
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
